b4fb91569e033231e8eec32b16e2c470d69f2fa0
Emanuele Mazza <n3m3da@d3cod3.org>
2018-06-01 01:16:34 +0200

    updated for OF 0.10.0

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,11 +1,11 @@
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
     int totalVertices = 5;
     float yInc = (ofGetHeight()-100)/totalVertices;
     for (int i = 0; i<= totalVertices; i++) {
-        points.push_back(ofVec3f(ofGetWidth()*0.5f, ofGetHeight() - (yInc * i + 50)));
+        points.push_back(ofDefaultVec3(ofGetWidth()*0.5f, ofGetHeight() - (yInc * i + 50),0));
         vertices.push_back(draggableVertex(points.back().x, points.back().y));
         ofFloatColor c; c.setHsb(ofRandom(1), 1, 1 );
         colors.push_back(c);
@@ -15,61 +15,76 @@ void testApp::setup(){
     fatLine.add(points, colors, weights);
     fatLine.printDebug();
     bDrawDebug = false;
-    
+
 }
 
 
 //--------------------------------------------------------------
-void testApp::update(){
-    
+void ofApp::update(){
+
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw(){
     ofBackground(255);
     fatLine.draw();
     for (int i = 0; i < vertices.size(); i++) {
         vertices[i].draw();
     }
     if (bDrawDebug) {
-        fatLine.drawDebug();       
+        fatLine.drawDebug();
     }
 
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
     switch (key) {
         case ' ':
             bDrawDebug ^= true;
             break;
-            
+
         default:
             break;
     }
 }
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){}
+void ofApp::keyReleased(int key){}
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y){}
+void ofApp::mouseMoved(int x, int y){}
 //--------------------------------------------------------------
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button){
     for (int i = 0; i < vertices.size(); i++) {
         vertices[i].drag(x, y);
-        points[i].set(vertices[i]);
+        points[i].x = vertices[i].x;
+        points[i].y = vertices[i].y;
         fatLine.updatePoint(i, points[i]);
     }
     fatLine.update();
 }
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
     for (int i = 0; i < vertices.size(); i++) {
         vertices[i].over(x, y);
     }
 }
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button){
+    for (int i = 0; i < vertices.size(); i++) {
+        vertices[i].bOver = false;
+    }
+    fatLine.update();
+}
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    for (int i = 0; i < vertices.size(); i++) {
+        vertices[i].bOver = false;
+    }
+    fatLine.update();
+}
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
     for (int i = 0; i < vertices.size(); i++) {
         vertices[i].bOver = false;
     }
@@ -77,8 +92,8 @@ void testApp::mouseReleased(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){}
+void ofApp::windowResized(int w, int h){}
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){}
+void ofApp::dragEvent(ofDragInfo dragInfo){}
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){}
+void ofApp::gotMessage(ofMessage msg){}
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -2,6 +2,7 @@
 
 #include "ofMain.h"
 #include "ofxFatLine.h"
+
 #define DRAG_SIZE 10
 class draggableVertex: public ofVec3f{
 public:
@@ -11,12 +12,12 @@ public:
         this->y=y;
         this->z=0;
         bOver = false;
-        
-    }    
+
+    }
     void over(float x, float y){
         bOver = (r.inside(x, y));
     }
-    
+
     void drag(float x, float y){
         if (bOver) {
             r.setFromCenter(this->x, this->y, DRAG_SIZE,DRAG_SIZE);
@@ -31,39 +32,41 @@ public:
         ofSetLineWidth(1);
         ofNoFill();
         ofSetColor(0);
-        ofRect(this->r);
+        ofDrawRectangle(this->r);
         ofPopMatrix();
         ofPopStyle();
-        
+
     }
-    
+
     bool bOver;
     ofRectangle r;
-}; 
+};
 
 
-
-class testApp : public ofBaseApp{
+class ofApp : public ofBaseApp{
 public:
     void setup();
     void update();
     void draw();
-    
+
     void keyPressed(int key);
     void keyReleased(int key);
-    void mouseMoved(int x, int y);
+    void mouseMoved(int x, int y );
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
-    
- 
+
+
     ofxFatLine fatLine;
     vector<draggableVertex> vertices;
-    vector<ofVec3f> points;
+    vector<ofDefaultVec3> points;
     vector<ofFloatColor> colors;
     vector<double> weights;
     bool bDrawDebug;
+    
 };
